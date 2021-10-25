678ea4e7f400c24db9b22516ce5049a4c1276da3
Richard Brayan <richardbrayan2009@gmail.com>
2021-10-25 13:44:32 -0300

    Project created

---
+++ src/main/java/com/curso/WebService_Spring_JPA_Hibernate/WebServiceSpringJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.curso.WebService_Spring_JPA_Hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceSpringJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceSpringJpaHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/WebService_Spring_JPA_Hibernate/WebServiceSpringJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.WebService_Spring_JPA_Hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceSpringJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
